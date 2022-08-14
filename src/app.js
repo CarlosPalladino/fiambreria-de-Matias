084a75c0247d8474d34b2c50823f2130b3dd1720
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2022-08-14 15:45:21 -0300

    menu burger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();  
+const public = path.resolve(__dirname, '../public');
  app.listen(2000,() => {console.log('el server esta en funcionamiento')});
  app.get('/', (req, res) => res.sendFile (__dirname + '/views/index.html'));
+ app.use(express.static(public)); 
