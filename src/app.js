1b9d76fc2aaa40993deb11555fa71e9d5d343b61
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2022-08-15 18:45:42 -0300

    home empezado y vista de contacto hecha

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const path = require('path');
 const express = require('express');
+const { application } = require('express');
 const app = express();  
 const public = path.resolve(__dirname, '../public');
+/// app.use ///
  app.listen(2000,() => {console.log('el server esta en funcionamiento')});
+
  app.get('/', (req, res) => res.sendFile (__dirname + '/views/index.html'));
+
+
  app.use(express.static(public)); 
+
+ app.get("/contacto", (req,res) => res.sendFile(path.resolve(__dirname, './views/contacto.html')));
