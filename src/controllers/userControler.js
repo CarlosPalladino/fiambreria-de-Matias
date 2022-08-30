6ca1d7c0e2cbc6348846627484bc53b8fd4b8b78
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2022-08-30 11:16:31 -0300

    userController avanzado + nuevos controladora vistas

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -1,1 +1,56 @@
-let 
+const { validationResult } = require('express-validator');
+const { index, one, create, write } = require("../models/usersModel");
+
+const { resolve } = require('path');
+const { isAdmin } = require('../middlewares/isAdmin');
+
+const userController = {
+    register = async (req, res) => {
+        return res.render("./users/register", {
+            title: "Registro",
+            styles: ["header", "footer", "register"]
+        })
+    },
+    process: async (req, res) => {
+        let validaciones = validationResult(req)
+        let { errors } = validaciones;
+
+        if (errors && errors.length > 0) {
+            return res.render("./users/register", {
+                title: "Registro",
+                styles: ["header", "footer", "register"],
+                errors: validaciones, mapped()
+            });
+        }
+
+
+        req.body.password = hashSync(req.body.password, 10);
+        req.body.isAdmin = String(req.body.email).toLocaleLowerCase().includes('@.com');
+        req.body.imagen = req.files[0].filename;
+
+        if (req.files && req.files.length > 0) {
+
+            let imagenUsuario = await imagen.create({
+                nombre: req.files[0].filename
+            })
+            req.body.imagenId = imagenUsuario.id;
+        }
+        await usuarios.create(req.body)
+        return res.redirect('/')
+    },
+    login: async function (req, res) {
+        return res.render('users/login', {
+            title: "Login",
+            styles: ["style", "header", "footer", "login"]
+        });
+    },
+
+    logout: async function (req, res) {
+        delete req.session.user
+        return res.redirect('/')
+    },
+
+}
+
+module.exports = controller
+
--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -1,4 +1,5 @@
-
+const {readFileSync,writeFileSync, fdatasync} = require('fs')
+const { resolve } = require('path')
 
 
 
@@ -9,24 +10,41 @@ index : function () {
     let data = readfleSync(file);
     return JSON.parse(data);
 },
-find: function () {
+one: function () {
 let file = path.join(__dirname, '../data', 'users.json');
+let data = readfleSync(file);
+let users = JSON.parse(data);
+return users.find (find=> users.email== users.email)
+},
 
+create : function (data) {
+let file = resolve(__dirname, '../data', 'users.json');
+let info = readFileSync(file);
+let users = JSON.parse(info)
+return users.find (find=> users.email== users.email)
+},
 
+write: function (data) {
+let file = resolve(__dirname, '../data', 'users.json');
+let info = JSON.stringify(data,null);
+return writeFileSync(file, info);
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+module.exports = model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
